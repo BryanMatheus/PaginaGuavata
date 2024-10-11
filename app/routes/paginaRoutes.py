96d56e5c3d0f6192052c810fe810952512a3e091
RStivenDev <158865834+XxSTVNxX@users.noreply.github.com>
2024-10-11 18:02:41 -0500

    DISEÑO INICIO

--- app/routes/paginaRoutes.py
+++ app/routes/paginaRoutes.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+from app.models.galeria import Galeria
 
 bp = Blueprint('pagina', __name__)
 
 @bp.route('/')
 def inicio():
-    return render_template("/pagina2/landingPage.html")
+    imagenes= Galeria.query.all()
+    return render_template("/pagina2/landingPage.html", imagenes=imagenes)
